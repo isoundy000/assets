3189899b0f2388fa486d1042f537c98205da3215
Johnny Wu <KarasAya@gmail.com>
2015-11-10 18:04:27 +0800

    update tests

--- test/renderer/basic.js
+++ test/renderer/basic.js
@@ -1,4 +1,10 @@
+'use strict';
+
+Editor.require('app://editor/page/page-init-for-test');
+
 describe('<editor-assets>', function() {
+    Helper.runPanel( 'assets.panel' );
+
     var deepQuery = sinon.stub( Editor.assetdb, 'deepQuery' );
     deepQuery.yields([{
         uuid: 'assets',
@@ -35,36 +41,35 @@ describe('<editor-assets>', function() {
         ],
     }]);
 
-    var assetsEL;
-    beforeEach(function ( done ) {
-        fixture('panel', function (el) {
-            assetsEL = el;
-            done();
-        });
-    });
-    after(function () {
-    });
-
     it('should load asset when ready', function( done ) {
-        expect( Polymer.dom(assetsEL.$.tree).children[0].name ).to.be.equal('assets');
-        expect( assetsEL.$.tree._id2el['149c6d29-119e-4ba7-84bd-1fd4ce1f46cd'].name ).to.be.equal('simple-texture');
+        let targetEL = Helper.targetEL;
+
+        expect( Polymer.dom(targetEL.$.tree).children[0].name ).to.be.equal('assets');
+        expect( targetEL.$.tree._id2el['149c6d29-119e-4ba7-84bd-1fd4ce1f46cd'].name ).to.be.equal('simple-texture');
         done();
     });
 
     it('should focus on search', function( done ) {
-        assetsEL.focusOnSearch();
-        expect( assetsEL.$.search.focused ).to.be.equal(true);
+        let targetEL = Helper.targetEL;
+
+        targetEL.focusOnSearch();
+        expect( targetEL.$.search.focused ).to.be.equal(true);
         done();
     });
 
     it('should select the item', function( done ) {
-        var fn = sinon.spy(assetsEL.$.tree, '_onItemSelect');
-        var itemEL = assetsEL.$.tree._id2el['1b984894-76c5-4687-b3b3-3f6f4e9f5f2c'];
-        Tester.click(itemEL);
+        let targetEL = Helper.targetEL;
 
-        expect(fn.calledOnce).to.be.equal(true);
+        let fn = sinon.spy(targetEL.$.tree, '_onItemSelect');
+        let itemEL = targetEL.$.tree._id2el['1b984894-76c5-4687-b3b3-3f6f4e9f5f2c'];
 
-        fn.restore();
-        done();
+        setTimeout(() => {
+            Helper.click(itemEL);
+
+            expect(fn.calledOnce).to.be.equal(true);
+
+            fn.restore();
+            done();
+        }, 10);
     });
 });
